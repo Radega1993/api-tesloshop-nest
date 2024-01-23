1bf0d309238434c5a1c6840cb3005e15ec148dab
Raul de Arriba <rauldearriba@gmail.com>
2024-01-23 11:35:34 +0100

    paginacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -15,6 +17,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    ProductsModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
